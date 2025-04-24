4676dd39d952a87082dc6b65e451850c740049f6
Mahnoor Fatima <aa004306@student.reading.ac.uk>
2025-04-24 20:16:41 +0100

    some updates made

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = 'c28918a56e4eeaaa980a877ea1a5f13e'  # Use the generated secret key directly
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
